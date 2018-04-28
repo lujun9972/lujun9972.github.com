5d98918d4478c528a959c07109e5545290001225
darksun <lujun9972@gmail.com>
2018-04-28 21:49:03 +0800

    fix link

--- generate_index.sh
+++ generate_index.sh
@@ -32,7 +32,7 @@ function generate_links()
         modify_date=$(git log --date=short --pretty=format:"%cd" -n 1 $post) # 去除日期前的空格
         if [[ -n "$modify_date" ]];then # 没有修改日期的文件没有纳入仓库中,不予统计
             postname=$(basename $post)
-            echo "+ [[https://github.com/lujun9972/lujun9972.github.io/blob/source/$post][$postname]]		<$modify_date>"
+            echo "+ [[https://github.com/lujun9972/lujun9972.github.com/blob/source/$post][$postname]]		<$modify_date>"
         fi
     done|sort -k 2
     IFS=$old_ifs
