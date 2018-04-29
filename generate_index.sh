c1a8b1ce845ab26a09238b17ee9e495f8689c546
darksun <lujun9972@gmail.com>
2018-04-29 22:20:53 +0800

    动态获取repo名称和当前branch

--- generate_index.sh
+++ generate_index.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+repo=$(basename $(git remote get-url origin) |sed 's/.git$//')
+branch=$(git branch |grep -E "^\*" |cut -c3-)
 
 declare -A catalog_comment_dict
 catalog_comment_dict=([Emacs之怒]="" [英文必须死]="" [life-hacking]="" [时间管理]="" [linux和它的小伙伴]="" [编程之旅]="" [无主之地]="" )
@@ -32,7 +34,7 @@ function generate_links()
         modify_date=$(git log --date=short --pretty=format:"%cd" -n 1 $post) # 去除日期前的空格
         if [[ -n "$modify_date" ]];then # 没有修改日期的文件没有纳入仓库中,不予统计
             postname=$(basename $post)
-            echo "+ [[https://github.com/lujun9972/lujun9972.github.com/blob/source/$post][$postname]]		<$modify_date>"
+            echo "+ [[https://github.com/lujun9972/${repo}/blob/${branch}/$post][$postname]]		<$modify_date>"
         fi
     done|sort -k 2
     IFS=$old_ifs
