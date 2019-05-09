fb2755699c7c6e8f039fd0cc3642102227d95091
darksun <lujun9972@gmail.com>
2019-05-09 21:25:44 +0800

    添加netlify標誌

--- generate_index.sh
+++ generate_index.sh
@@ -40,6 +40,11 @@ function generate_links()
     IFS=$old_ifs
 }
 
+echo <<EOF
+#+ATTR_HTML :alt Netlify Status
+[[https://app.netlify.com/sites/thirsty-pike-764a9b/deploys][https://api.netlify.com/api/v1/badges/3b4ebb33-1ce2-4238-9a69-e4ecdafd2f1a/deploy-status.png]]
+
+EOF
 
 for catalog in $catalogs
 do
